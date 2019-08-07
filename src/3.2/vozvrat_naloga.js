845e02bad5655abd4a73fe723487e0dfb8e23828
aleksandrap0789 <aleksandrap.0789@gmail.com>
2019-08-08 00:50:11 +0300

    third week practice. task 2. js completed

--- src/3.2/vozvrat_naloga.js
+++ src/3.2/vozvrat_naloga.js
@@ -1,4 +1,12 @@
-'use strict'
+let option = document.querySelector(".options");
+option.onclick = function(event) {
+    let target = event.target.closest('.option-content');
+    let icon = target.querySelector(".option-content-icon");
+    if (!target) return;
+    target.classList.toggle('change_color');
+    icon.classList.toggle('change_icon');
+};
+
 
 function checkbox(i) {
     let inputs = document.querySelectorAll(".input");
@@ -6,42 +14,18 @@ function checkbox(i) {
     let box = document.querySelectorAll(".boxes-content");
     let box_img = document.querySelectorAll(".boxes-content img");
 
-    if(inputs[i].checked){
+    if (inputs[i].checked) {
         labels[i].style.backgroundImage = "url('check.svg')";
-        if (i !== (inputs.length - 1)){
+        if (i !== (inputs.length - 1)) {
             box[i].style.borderColor = "#19bb4f";
             box_img[i].style.filter = "none";
         }
 
-    }
-    else{
+    } else {
         labels[i].style.backgroundImage = "";
-        if (i !== (inputs.length - 1)){
+        if (i !== (inputs.length - 1)) {
             box[i].style.borderColor = "#d8d8d8";
             box_img[i].style.filter = "grayscale(100%) opacity(25%)";
         }
-
     }
-
-    let option = document.querySelector(".options");
-    let content = document.querySelector(".option-content");
-    let selectedTd;
-
-    table.onclick = function(event) {
-        let target = event.target.closest('.option-content');
-
-        if (!target) return;
-        if (option.contains(target)) return;
-
-       changeTarget(target);
-    };
-
-    function changeTarget(target) {
-        if (selectedTd) { // убрать существующую подсветку, если есть
-            selectedTd.classList.remove('change_color');
-        }
-        selectedTd = target;
-        selectedTd.classList.add('change_color'); // подсветить новый td
-    }
-
-};
+}
